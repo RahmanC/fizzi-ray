a041be43e538251dad2bcaa0b5f9f64374b5aa7c
rhammy <afaraeturahman@gmail.com>
2024-10-04 12:03:32 +0100

    ft:Reusable soda can with drei

--- src/components/ViewCanvas.tsx
+++ src/components/ViewCanvas.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
 
 type Props = {};
 
@@ -22,13 +23,7 @@ export default function ViewCanvas({}: Props) {
         fov: 30,
       }}
     >
-      {/* MATH PI * 2 => 2PI  */}
-      <mesh rotation={[0.5, 0.5, 0]} position={[1, 0, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color={"hotpink"} />
-      </mesh>
-      <ambientLight intensity={2} />
-      <spotLight intensity={3} position={[1, 1, 1]} />
+      <View.Port />
     </Canvas>
   );
 }
